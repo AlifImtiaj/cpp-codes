0efef04dbb138d426f07dcb7352b2d89d360e688
Alif Imtiaj <lolalif412@gmail.com>
2024-06-06 18:46:49 +0600

    primitive data types

---
+++ data_types.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    
+    // INTEGER
+    // int i = 10;
+    int i = 'A';
+    cout << i << endl;
+
+    // FLOAT
+    float f = 3.14f;
+    cout << f << endl;
+
+    // DOUBLE
+    double d = 5.00011555;
+    cout << d << endl;
+
+    // CHARACTER
+
+    // char ch = 'A';
+    char ch = 65;
+    cout << ch << endl;
+
+    // BOOLEAN
+    // bool b = 1;
+    bool b = true;
+    cout << b << endl;
+
+
+}
